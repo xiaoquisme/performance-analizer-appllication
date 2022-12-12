bcce05bb477e664a1f4eb67672d342c156bd7de4
XiaoQu <1483523635@qq.com>
2022-12-12 12:32:09 +0800

    [XiaoQu] #N/A feat: init project

---
+++ src/main/java/com/example/performanceanalizerapplication/PerformanceAnalizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.performanceanalizerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceAnalizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerformanceAnalizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/performanceanalizerapplication/PerformanceAnalizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.performanceanalizerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceAnalizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
