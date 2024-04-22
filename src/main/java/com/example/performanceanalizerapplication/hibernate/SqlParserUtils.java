f72fab32bc458f26575fe1012845259e02381b17
XiaoQu <1483523635@qq.com>
2024-04-22 22:39:22 +0800

    feat: enable sub select replace

--- src/main/java/com/example/performanceanalizerapplication/hibernate/SqlParserUtils.java
+++ src/main/java/com/example/performanceanalizerapplication/hibernate/SqlParserUtils.java
@@ -6,6 +6,7 @@ import org.apache.calcite.sql.*;
 import org.apache.calcite.sql.dialect.MysqlSqlDialect;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
+import org.jetbrains.annotations.NotNull;
 
 public class SqlParserUtils {
   public static String parseSql(String sql) {
@@ -14,14 +15,13 @@ public class SqlParserUtils {
       try {
           SqlNode sqlNode = sqlParser.parseStmt();
           if (sqlNode.getKind() == SqlKind.SELECT) {
-              SqlSelect sqlSelect = (SqlSelect) sqlNode;
-              if (sqlSelect.getFrom().getKind() == SqlKind.AS) {
-                  SqlBasicCall basicCall = (SqlBasicCall) sqlSelect.getFrom();
-                  SqlIdentifier tableName = (SqlIdentifier) basicCall.getOperands()[0];
-                  String s = tableName.names.get(0);
-                  String newTableName = String.format("%s_%s",s, DatetimeUtils.currentDay.get());
-                  SqlIdentifier newTable = tableName.setName(0, newTableName);
-                  basicCall.setOperand(0, newTable);
+              SqlSelect sqlSelect = getSqlSelect((SqlSelect) sqlNode);
+              if(sqlSelect.getWhere().getKind() == SqlKind.IN) {
+                    SqlBasicCall basicCall = (SqlBasicCall) sqlSelect.getWhere();
+                    SqlNode sqlNode1 = basicCall.getOperands()[1];
+                    if(sqlNode1.getKind() == SqlKind.SELECT) {
+                       getSqlSelect((SqlSelect) sqlNode1);
+                    }
               }
           }
           return sqlNode.toSqlString(MysqlSqlDialect.DEFAULT).getSql();
@@ -29,5 +29,18 @@ public class SqlParserUtils {
           throw new RuntimeException(e);
       }
   }
+
+    private static @NotNull SqlSelect getSqlSelect(SqlSelect sqlNode) {
+        SqlSelect sqlSelect = sqlNode;
+        if (sqlSelect.getFrom().getKind() == SqlKind.AS) {
+            SqlBasicCall basicCall = (SqlBasicCall) sqlSelect.getFrom();
+            SqlIdentifier tableName = (SqlIdentifier) basicCall.getOperands()[0];
+            String s = tableName.names.get(0);
+            String newTableName = String.format("%s_%s",s, DatetimeUtils.currentDay.get());
+            SqlIdentifier newTable = tableName.setName(0, newTableName);
+            basicCall.setOperand(0, newTable);
+        }
+        return sqlSelect;
+    }
 
 }
